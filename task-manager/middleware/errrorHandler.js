ab438abc25c777ee5312a93d55ffa4512726f1e8
Praduman Singh Chundawat <pschundawat4080@gmail.com>
2023-10-17 02:15:40 +0530

    Error handler changes

--- task-manager/middleware/errrorHandler.js
+++ task-manager/middleware/errrorHandler.js
@@ -4,7 +4,9 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message });
   }
-  return res.status(err.status).json({ msg: err.message });
+  return res
+    .status(500)
+    .json({ msg: "Something went wrong, please try again" });
 };
 
 module.exports = errorHandlerMiddleware;
