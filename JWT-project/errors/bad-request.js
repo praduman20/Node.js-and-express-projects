4c8d3edd5fe4ba719b70b7995f73f5626351f7eb
Praduman Singh Chundawat <pschundawat4080@gmail.com>
2023-10-25 21:18:54 +0530

    Added jwt and controller logic

---
+++ JWT-project/errors/bad-request.js
@@ -0,0 +1,11 @@
+const CustomApiError = require("../errors/custom-error");
+const { StatusCodes } = require("http-status-codes");
+
+class BadRequest extends CustomApiError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
+module.exports = BadRequest;
